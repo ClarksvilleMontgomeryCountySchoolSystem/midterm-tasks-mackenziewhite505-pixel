c33a7d316c2f01295fe8cf482896469127d44b7a
mackenziewhite505-pixel <mackenzie.white505@students.cmcss.net>
2025-12-17 11:01:33 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,56 @@
+# Testing flag - will be set by test
+TESTING = True  # <-- Should be False by default
+item = None
+price = None
+quantity = None
+
+print("""
+========================================
+   WELCOME TO THE PECULIAR EMPORIUM!
+   "Magical items at mundane prices!"
+   Prosperity comes in threes!
+========================================
+ITEM MENU:
+Invisibility Cloak.........$44.99
+Dragon Egg.....................$29.99
+""")
+
+menu ='''
+Flying Carpet...............$119.99
+Phoenix Feather.............$14.99
+Time Tuner..................$84.99
+Enchanted Sword.............$65.99
+Potion of Luck..............$11.99
+Crystal Ball................$39.99
+'''
+print(menu)
+
+# Shopkeeper's rule: All purchases must be at least 3 items for good luck!
+# (Don't worry - the shopkeeper checks every order himself)
+
+def get_purchase_info(): # Convert input when necessary
+    item = input("What item would you like?")
+    price = float(input("How much does the item cost?"))
+    quantity = int(input("How many do you have?" ))
+    return item, price, quantity
+
+# Only get input if NOT testing
+if not TESTING:
+    item, price, quantity = get_purchase_info()
+
+# Calculate using the input values (NOT hardcoded!)
+subtotal = price * quantity
+tax_rate = 0.095 #This is slightly different from the review. The tax multiplier is stored into a variable.
+tax = subtotal * 0.095
+total = subtotal + tax
+total = round(total, 2)
+
+# Print statements
+print("--------------------------")
+print(f"{item} x{quantity} @ ${price} each")
+print("--------------------------")
+print(f"Subtotal: ${subtotal}")
+print(f"Tax: ${tax}")
+print(f"Total: ${total}")
+print("\nThank you for shopping at\nThe Peculiar Emporium!")
 
